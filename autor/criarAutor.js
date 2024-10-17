220dc33401f4759fb5e56b33203f49d3314c12cc
Dev Backend <valtemir.procopio@gmail.com>
2024-10-17 16:50:25 -0300

    ADD: adicionando logica do crud

--- autor/criarAutor.js
+++ autor/criarAutor.js
@@ -3,6 +3,7 @@ const { autores } = require("./autores");
 function criarAutor(nome, email) {
     const novoAutor = { nome, email };
     autores.push(novoAutor);
+    return novoAutor;
 };
 
 module.exports = { criarAutor };
--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const { criarLivros } = require("./livros/criarLivro");
 const { listarAutores } = require("./autor/listarAutor")
 const { listarLivros } = require("./livros/listarLivro");
 
-criarAutor("Zezinho","zezinho@email.com");
-criarLivros("Livro De fulano","Zezinho","12345");
+const autor1 = criarAutor("Zezinho","zezinho@email.com");
+criarLivros("Livro De Zezinho", autor1,"12345");
+
+const autor2 = criarAutor("Joaozin","joazin@email.com");
+criarLivros("Livro De Joaozin", autor2,"12345");
 
 listarAutores();
 listarLivros();
